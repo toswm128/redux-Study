831a29d0187051d2ecd41b687d6f958a52e409f3
조민수 <chominsu10@gmail.com>
2021-05-03 16:07:35 +0900

    [ADD] TEST

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,21 +1,20 @@
-import React from 'react';
+import React from "react";
 
-const Counter = ({
-  number,
-  onIncrement,
-  onDecrement
-}) => {
+const Counter = ({ number, onIncrement, onDecrement, onTest, String }) => {
   return (
     <div>
       <h1>{number}</h1>
       <button onClick={onIncrement}>증가 (+)</button>
       <button onClick={onDecrement}>감소 (-)</button>
+      <button onClick={onTest}>test</button>
+      <h1>{String}</h1>
     </div>
   );
 };
 
 Counter.defaultProps = {
-  number: 0
-}
+  number: 0,
+  testS: "null",
+};
 
 export default Counter;
--- src/containers/CounterContainer.js
+++ src/containers/CounterContainer.js
@@ -6,35 +6,48 @@ import * as counterActions from "../store/modules/counter";
 class CounterContainer extends Component {
   handleIncrement = () => {
     this.props.increment();
+    console.log(this.props);
   };
 
   handleDecrement = () => {
     this.props.decrement();
   };
 
+  handleTest = () => {
+    this.props.test();
+  };
+
   render() {
-    const { handleIncrement, handleDecrement } = this;
-    const { number } = this.props;
+    const { handleIncrement, handleDecrement, handleTest } = this;
+    const { number, testS } = this.props;
+    console.log(this);
 
     return (
       <Counter
         onIncrement={handleIncrement}
         onDecrement={handleDecrement}
+        onTest={handleTest}
         number={number}
+        String={testS}
       />
     );
   }
 }
 
 // props 값으로 넣어 줄 상태를 정의해줍니다.
-const mapStateToProps = state => ({
-  number: state.counter.number,
-});
+const mapStateToProps = state => (
+  console.log(state),
+  {
+    number: state.counter.number,
+    testS: state.counter.testS,
+  }
+);
 
 // props 값으로 넣어 줄 액션 함수들을 정의해줍니다
 const mapDispatchToProps = dispatch => ({
   increment: () => dispatch(counterActions.increment()),
   decrement: () => dispatch(counterActions.decrement()),
+  test: () => dispatch(counterActions.test()),
 });
 
 // 컴포넌트를 리덕스와 연동 할 떄에는 connect 를 사용합니다.
